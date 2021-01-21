6014a2cd438f67e4285b302e8dd8c6a9899452eb
aldorojas <64167239+aldorojas@users.noreply.github.com>
2021-01-21 16:46:21 -0600

    observaciones V1.1

--- frontend/assets/js/login.js
+++ frontend/assets/js/login.js
@@ -66,8 +66,8 @@ formLogin.addEventListener('submit', function(e){
 			localStorage.setItem("idHospital",data.Medico.id_hospital);
 
 
-			//window.location.replace("http://134.122.120.195:8080/modulos/encuentros.html");
-			window.location.replace("http://127.0.0.1:5500/modulos/inicio.html");
+			window.location.replace("http://134.122.120.195:8080/modulos/inicio.html");
+			//window.location.replace("http://127.0.0.1:5500/modulos/inicio.html");
 
 
 		}
